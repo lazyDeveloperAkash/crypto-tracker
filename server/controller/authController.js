a555305057678291a37fb0c04b0879ffccaf066f
lazyDeveloperAkash@gmail.com <lazyDeveloperAkash@gmail.com>
2025-07-13 11:26:23 +0530

    config cookies

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -27,7 +27,7 @@ const register = async (req, res) => {
     // Set cookie
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: process.env.NODE_ENV === "development" ? false : true,
       sameSite: "none",
       maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
     })
